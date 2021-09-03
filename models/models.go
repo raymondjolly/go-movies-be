6280cac474a2f3513094bcd8bcc41a1ef95dee20
Raymond Jolly <raymond.jolly@gmail.com>
2021-09-03 16:59:34 -0400

    Added a models directory and structs to capture Movie, Genre, and MovieGenre.

---
+++ models/models.go
@@ -0,0 +1,33 @@
+package models
+
+import "time"
+
+type Movie struct {
+	Id          int          `json:"id"`
+	Title       string       `json:"title"`
+	Description string       `json:description`
+	Year        int          `json:"year"`
+	ReleaseDate time.Time    `json:"release_date"`
+	RunTime     int          `json:"run_time"`
+	Rating      int          `json:"rating"`
+	MPAARating  string       `json:"mpaa_rating"`
+	CreatedAt   time.Time    `json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
+	MovieGenre  []MovieGenre `json:"-"`
+}
+
+type Genre struct {
+	Id        int       `json:"id"`
+	GenreName string    `json:"genre_name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type MovieGenre struct {
+	Id        int       `json:"id"`
+	MovieId   int       `json:"movie_id"`
+	GenreId   int       `json:"genre_id"`
+	Genre     string    `json:"genre"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}

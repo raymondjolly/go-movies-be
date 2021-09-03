40ba28d1d3454eb5d862333595ac447678ea1d3f
Raymond Jolly <raymond.jolly@gmail.com>
2021-09-03 16:47:50 -0400

    Added the julien schmidt router and cleaned up code to add configuration and application structs for a cleaner setup.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type AppStatus struct {
@@ -21,6 +22,11 @@ type config struct {
 	env  string
 }
 
+type application struct {
+	config config
+	logger *log.Logger
+}
+
 func main() {
 	var cfg config
 
@@ -28,24 +34,23 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "Application environment: (development | production")
 	flag.Parse()
 
-	fmt.Println("Running")
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		currentStatus := AppStatus{
-			Status:      "Available",
-			Environment: cfg.env,
-			Version:     version,
-		}
-		js, err := json.MarshalIndent(currentStatus, "", "\t")
-		if err != nil {
-			log.Println(err)
-		}
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
 
-		w.Header().Set("Content-Type", "application/json ")
-		w.WriteHeader(http.StatusOK)
-		w.Write(js)
+	srv := http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute * 1,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+	}
 
-	})
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+	logger.Println("Starting server on port: ", app.config.port)
+	err := srv.ListenAndServe()
+
 	if err != nil {
 		log.Fatalln(err)
 	}

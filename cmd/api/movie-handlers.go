f899ceddb2dd3cd862a99977a740a2140e696056
Raymond Jolly <raymond.jolly@gmail.com>
2021-09-17 00:47:34 -0400

    Created an insert movie function so that Movie data can be submitted to the database. The EditMovie handler was also modified so that the insert function can be called to perform this action.

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
+	"go-movies-be/models"
+	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
+
+type jsonResp struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
 
 func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	params := httprouter.ParamsFromContext(r.Context())
@@ -54,23 +63,56 @@ func (app *application) getMoviesByGenre(w http.ResponseWriter, r *http.Request)
 	err = app.writeJSON(w, http.StatusOK, movies, "movies")
 }
 
+type MoviePayload struct {
+	Id          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Year        string `json:"year"`
+	ReleaseDate string `json:"release_date"`
+	RunTime     string `json:"runtime"`
+	Rating      string `json:"rating"`
+	MPAARating  string `json:"mpaa_rating"`
+}
+
 func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
-	type jsonResp struct {
-		OK bool `json:"ok"`
+	var payload MoviePayload
+
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	var movie models.Movie
+
+	movie.Id, _ = strconv.Atoi(payload.Id)
+	movie.Title = payload.Title
+	movie.Description = payload.Description
+	movie.Rating, _ = strconv.Atoi(payload.Rating)
+	movie.ReleaseDate, _ = time.Parse("2006-01-02", payload.ReleaseDate)
+	movie.RunTime, _ = strconv.Atoi(payload.RunTime)
+	movie.Year = movie.ReleaseDate.Year()
+	movie.MPAARating = payload.MPAARating
+	movie.CreatedAt = time.Now()
+	movie.UpdatedAt = time.Now()
+
+	err = app.Models.DB.InsertMovie(movie)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
 	}
 
 	ok := jsonResp{
 		OK: true,
 	}
 
-	err := app.writeJSON(w, http.StatusOK, ok, "response")
+	err = app.writeJSON(w, http.StatusOK, ok, "response")
 	if err != nil {
 		app.errorJSON(w, err)
 	}
 }
 
 //TODO write a handler for a deleteMovie function
-
-//TODO write a handler for an insertMovie function
 
 //TODO write a handler for a searchMovie function
--- models/movies-db.go
+++ models/movies-db.go
@@ -164,3 +164,19 @@ func (m *DBmodel) GenresAll() ([]*Genre, error) {
 	}
 	return genres, nil
 }
+
+func (m *DBmodel) InsertMovie(movie Movie) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `insert into movies(title, description, year, release_date, runtime, rating, mpaa_rating, created_at,
+		updated_at) values ($1,$2,$3,$4,$5,$6,$7,$8,$9)`
+
+	_, err := m.DB.ExecContext(ctx, stmt, movie.Title, movie.Description, movie.Year, movie.ReleaseDate, movie.RunTime,
+		movie.Rating, movie.MPAARating, movie.CreatedAt, movie.UpdatedAt)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}

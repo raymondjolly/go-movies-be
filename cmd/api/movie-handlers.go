d6a171507747880ac82de0c38a42126aed05de2a
Raymond Jolly <raymond.jolly@gmail.com>
2021-09-19 11:40:41 -0400

    Created an update movie handler to the backend

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/julienschmidt/httprouter"
+	"errors"
 	"go-movies-be/models"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 type jsonResp struct {
@@ -17,8 +19,10 @@ type jsonResp struct {
 
 func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	params := httprouter.ParamsFromContext(r.Context())
+
 	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil {
+		app.logger.Print(errors.New("invalid id parameter"))
 		app.errorJSON(w, err)
 		return
 	}
@@ -28,6 +32,7 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
 	if err != nil {
 		app.errorJSON(w, err)
+		return
 	}
 }
 
@@ -35,46 +40,73 @@ func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 	movies, err := app.Models.DB.All()
 	if err != nil {
 		app.errorJSON(w, err)
+		return
 	}
+
 	err = app.writeJSON(w, http.StatusOK, movies, "movies")
 	if err != nil {
 		app.errorJSON(w, err)
+		return
 	}
+
 }
 
 func (app *application) getAllGenres(w http.ResponseWriter, r *http.Request) {
 	genres, err := app.Models.DB.GenresAll()
 	if err != nil {
 		app.errorJSON(w, err)
+		return
 	}
+
 	err = app.writeJSON(w, http.StatusOK, genres, "genres")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
-func (app *application) getMoviesByGenre(w http.ResponseWriter, r *http.Request) {
+func (app *application) getAllMoviesByGenre(w http.ResponseWriter, r *http.Request) {
 	params := httprouter.ParamsFromContext(r.Context())
-	genreId, err := strconv.Atoi(params.ByName("genre_id"))
+
+	genreID, err := strconv.Atoi(params.ByName("genre_id"))
 	if err != nil {
 		app.errorJSON(w, err)
+		return
 	}
-	movies, err := app.Models.DB.All(genreId)
+
+	movies, err := app.Models.DB.All(genreID)
 	if err != nil {
 		app.errorJSON(w, err)
+		return
 	}
+
 	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+
 }
 
 type MoviePayload struct {
-	Id          string `json:"id"`
+	ID          string `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Year        string `json:"year"`
 	ReleaseDate string `json:"release_date"`
-	RunTime     string `json:"runtime"`
+	Runtime     string `json:"runtime"`
 	Rating      string `json:"rating"`
 	MPAARating  string `json:"mpaa_rating"`
 }
 
-func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
+func (app *application) editmovie(w http.ResponseWriter, r *http.Request) {
 	var payload MoviePayload
 
 	err := json.NewDecoder(r.Body).Decode(&payload)
@@ -86,19 +118,32 @@ func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 
 	var movie models.Movie
 
-	movie.Id, _ = strconv.Atoi(payload.Id)
+	if payload.ID != "0" {
+		id, _ := strconv.Atoi(payload.ID)
+		m, _ := app.Models.DB.Get(id)
+		movie = *m
+		movie.UpdatedAt = time.Now()
+	}
+
+	movie.Id, _ = strconv.Atoi(payload.ID)
 	movie.Title = payload.Title
 	movie.Description = payload.Description
-	movie.Rating, _ = strconv.Atoi(payload.Rating)
 	movie.ReleaseDate, _ = time.Parse("2006-01-02", payload.ReleaseDate)
-	movie.RunTime, _ = strconv.Atoi(payload.RunTime)
 	movie.Year = movie.ReleaseDate.Year()
+	movie.RunTime, _ = strconv.Atoi(payload.Runtime)
+	movie.Rating, _ = strconv.Atoi(payload.Rating)
 	movie.MPAARating = payload.MPAARating
 	movie.CreatedAt = time.Now()
 	movie.UpdatedAt = time.Now()
 
 	if movie.Id == 0 {
 		err = app.Models.DB.InsertMovie(movie)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+	} else {
+		err = app.Models.DB.UpdateMovie(movie)
 		if err != nil {
 			app.errorJSON(w, err)
 			return
@@ -112,6 +157,7 @@ func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 	err = app.writeJSON(w, http.StatusOK, ok, "response")
 	if err != nil {
 		app.errorJSON(w, err)
+		return
 	}
 }
 
--- models/movies-db.go
+++ models/movies-db.go
@@ -180,3 +180,19 @@ func (m *DBmodel) InsertMovie(movie Movie) error {
 	}
 	return nil
 }
+
+func (m *DBmodel) UpdateMovie(movie Movie) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `update movies set title =$1, description =$2, year=$3, release_date=$4, runtime=$5, rating=$6, mpaa_rating=$7,
+		updated_at=$8 where id=$9`
+
+	_, err := m.DB.ExecContext(ctx, stmt, movie.Title, movie.Description, movie.Year, movie.ReleaseDate, movie.RunTime,
+		movie.Rating, movie.MPAARating, movie.UpdatedAt, movie.Id)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
